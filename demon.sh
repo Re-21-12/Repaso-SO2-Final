2e9d600410b35add1e3d19c2b64e699314ab2542
Re-21-12 <victora.caas@gmail.com>
2025-06-10 00:04:26 -0600

    [INIT] : init proy

---
+++ demon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script del demonio
+while true; do
+    # Tu lógica aquí
+    echo "$(date): Demonio ejecutándose..." >> /home/w-didnt-work/Desktop/hello-kitty/Demon/mi-demonio.log
+    sleep 30
+done
